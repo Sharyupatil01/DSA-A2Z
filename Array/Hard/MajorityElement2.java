4583893d27c2a9c0a97e3cc9c6dd14103d40c52f
Sharyupatil01 <sharyu2701@gmail.com>
2025-10-10 17:46:19 +0530

    Added the optimal Approach for majority Element 2

--- Array/Hard/MajorityElement2.java
+++ Array/Hard/MajorityElement2.java
@@ -11,20 +11,76 @@ public class MajorityElement2{
 
         }
     
-        HashMap<Integer,Integer> hash=new HashMap<>();
+        // HashMap<Integer,Integer> hash=new HashMap<>();
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     hash.put(arr[i],hash.getOrDefault(arr[i],0)+1);
+        // }
+        // ArrayList<Integer> result=new ArrayList<>();
+        // for(Map.Entry<Integer,Integer> hm:hash.entrySet())
+        // {
+        //     if(hm.getValue()>arr.length/3)
+        //     {
+        //         result.add(hm.getKey());
+        //     }
+        // }
+        // System.out.println("Result :"+ result);
+
+        int  count1=0;
+        int count2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++)
         {
-            hash.put(arr[i],hash.getOrDefault(arr[i],0)+1);
-        }
-        ArrayList<Integer> result=new ArrayList<>();
-        for(Map.Entry<Integer,Integer> hm:hash.entrySet())
-        {
-            if(hm.getValue()>arr.length/3)
+            if(count1==0 && arr[i]!=ele2)
             {
-                result.add(hm.getKey());
+                count1=1;
+                ele1=arr[i];
+
+            }
+            else if (count2==0 && arr[i]!=ele1)
+            {
+                count2=1;
+                ele2=arr[i];
+            }
+            else if(arr[i]==ele1)
+            {
+                count1=count1+1;
+            }
+            else if(arr[i]==ele2)
+            {
+                count2=count2+1;
+            }
+            else
+            {
+                count1=count1-1;
+                count2=count2-1;
             }
         }
-        System.out.println("Result :"+ result);
+        count1=0;
+        count2=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(ele1==arr[i])
+            {
+                count1++;
+            }
+            if(ele2==arr[i])
+            {
+                count2++;
+            }
+        }
+        List<Integer> result=new ArrayList<>();
+        if(count1>arr.length/3)
+        {
+            result.add(ele1);
+        }
+        if(count2>arr.length/3)
+        {
+            result.add(ele2);
+        }
+        System.out.println(result);
+        
 
     }
 }
