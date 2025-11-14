a0e7a559157dcff877d96f308590a96d91b86939
Sharyupatil01 <sharyu2701@gmail.com>
2025-11-14 23:11:00 +0530

    Example 3  Added

---
+++ Array/Easy/ToggleCharacters.java
@@ -0,0 +1,26 @@
+package Array.Easy;
+
+public class ToggleCharacters {
+    public static void main(String args[])
+    {
+        String str="HeLLo WoRLd";
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                sb.append((char)(ch-32));
+            }
+            else if(ch>='A' && ch<='Z')
+            {
+                sb.append((char)(ch+32));
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
