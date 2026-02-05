721df0aeca4806aeba222a9c28e1054b2f79a462
Sharyu Patil <139983419+Sharyupatil01@users.noreply.github.com>
2026-02-05 09:25:47 +0530

    Time: 1305 ms (16.27%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,16 +1,30 @@
 # Write your MySQL query statement below
 
-select Department , Employee , Salary 
-from(
-    select d.name as Department , 
-           e.name as Employee,
-           e.salary as Salary ,
-           dense_rank() over(
-             PARTITION  by e.departmentId 
-            order by e.salary desc 
-           ) as rankk 
-           from Employee as e 
-           join Department as d 
-           on e.departmentId=d.id 
-) t 
-where rankk<=3;
+-- select Department , Employee , Salary 
+-- from(
+--     select d.name as Department , 
+--            e.name as Employee,
+--            e.salary as Salary ,
+--            dense_rank() over(
+--             PARTITION  by e.departmentId 
+--             order by e.salary desc 
+--            ) as rankk 
+--            from Employee as e 
+--            join Department as d 
+--            on e.departmentId=d.id 
+-- ) t 
+-- where rankk<=3;
+
+
+select d.name as Department , 
+       e.name as Employee ,
+       e.salary as Salary 
+from Employee as e 
+join Department as d 
+on e.departmentId = d.id 
+where 3 >(
+    select count(distinct e1.salary)
+    from Employee as e1 
+    where e1.departmentId=e.departmentId 
+    and  e1.salary > e.salary
+);
